8dd19f85dfb860acb3bba6e0307107dd7b090715
toterGott <alex7lf@gmail.com>
2021-09-22 23:16:53 +0300

    Add LongestSubstring

---
+++ src/com/leetpractice/LongestSubstring.java
@@ -0,0 +1,34 @@
+package com.leetpractice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstring {
+
+    public static void main(String[] args) {
+        System.out.println(countLongestSubstring("ababcabcde"));
+        System.out.println(countLongestSubstring("ababc"));
+        System.out.println(countLongestSubstring("abbca"));
+        System.out.println(countLongestSubstring("abbca"));
+    }
+
+    private static Integer countLongestSubstring(String input) {
+        Map<Character, Integer> charMap = new HashMap();
+        int maxLength = 0;
+        int start = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (charMap.containsKey(input.charAt(i))) {
+                maxLength = Math.max(charMap.size(), maxLength);
+                maxLength = Math.max(i - start, maxLength);
+                int foundId = charMap.get(input.charAt(i));
+                while (start <= foundId) {
+                    charMap.remove(input.charAt(start++));
+                }
+            }
+            charMap.put(input.charAt(i), i);
+        }
+
+        return Math.max(charMap.size(), maxLength);
+    }
+}
